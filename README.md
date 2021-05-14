0cead2ea192514e9be986067fbf18aed66adc398
c4arl0s <c.santiago.cruz@gmail.com>
2021-05-14 09:58:33 -0500

    Indent code to improve readability

--- README.md
+++ README.md
@@ -1,2 +1,48 @@
 # renameAllFilesScript
 This script rename all files and directories in the current directory.
+This a new version for my script Script-renombrando-v.1.0
+
+```bash
+# find .        Busca en el directorio actual.
+# -name "*"     Busca todos los archivos
+# -type         de tipo 
+# f             file
+# d             directory
+# |             pipe
+# sort -n -r    ordena los archivos con opcion -n -r
+# while read    mientras lea todos los parametros que encuentra find, hace lo posterior.
+
+# renombrando primero archivos
+
+find . -name "*" -type f | sort -n -r | while read files    
+do
+    echo "Primero renombraremos archivos"
+    echo "First letter capitalized"
+    firstLetterCapitalized=`echo $file | awk '{for (i=1;i<=NF;i++) $i=toupper(substr($i,1,1)) substr($i,2)} 1'`
+    echo "archivos sin espacios, guiones bajos y de mayusculas a minisculas"
+    nameFiles_procesados=`echo $firstLetterCapitalized | tr ' ' '-' | tr '_' '-'`
+    
+    if [ $nameFiles_procesados == $file ]
+    then
+        echo "$CYAN $file yadirectoryocesado ... sin cambios"
+    else
+        mv "$file" "$nameFiles_procesados"
+        echo "renombrando archivo: $VERDE $file a $ROJO $nameFiles_procesados"
+    fi
+done
+
+# renombrando despues directorios
+
+find . -name "*" -type d | sort -n -r | while read directory    
+do
+    dirs_procesados=`echo $directory | tr ' ' '-' | tr '_' '-' | tr '[a-z]' '[A-Z]'`
+    if [ $dirs_procesados == $directory ]
+    then
+        echo "directorio: $CYAN $directory ya esta procesado ... sin cambios"
+    else
+        mv "$directory" "$dirs_procesados"
+        echo "renombrando Directorio: $VERDE $directory a $ROJO $dir_procesados"
+    fi
+done
+
+```
--- renameAllFiles.sh
+++ renameAllFiles.sh
@@ -10,32 +10,32 @@
 # renombrando primero archivos
 
 find . -name "*" -type f | sort -n -r | while read files    
-    do
-        echo "Primero renombraremos archivos"
-				echo "First letter capitalized"
-				firstLetterCapitalized=`echo $file | awk '{for (i=1;i<=NF;i++) $i=toupper(substr($i,1,1)) substr($i,2)} 1'`
-        echo "archivos sin espacios, guiones bajos y de mayusculas a minisculas"
-        nameFiles_procesados=`echo $firstLetterCapitalized | tr ' ' '-' | tr '_' '-'`
-        
-				if [ $nameFiles_procesados == $file ]
-        	then
-            	echo "$CYAN $file yadirectoryocesado ... sin cambios"
-        	else
-            	mv "$file" "$nameFiles_procesados"
-            	echo "renombrando archivo: $VERDE $file a $ROJO $nameFiles_procesados"
-        fi
-    done
+do
+    echo "Primero renombraremos archivos"
+    echo "First letter capitalized"
+    firstLetterCapitalized=`echo $file | awk '{for (i=1;i<=NF;i++) $i=toupper(substr($i,1,1)) substr($i,2)} 1'`
+    echo "archivos sin espacios, guiones bajos y de mayusculas a minisculas"
+    nameFiles_procesados=`echo $firstLetterCapitalized | tr ' ' '-' | tr '_' '-'`
+    
+    if [ $nameFiles_procesados == $file ]
+    then
+        echo "$CYAN $file yadirectoryocesado ... sin cambios"
+    else
+        mv "$file" "$nameFiles_procesados"
+        echo "renombrando archivo: $VERDE $file a $ROJO $nameFiles_procesados"
+    fi
+done
 
 # renombrando despues directorios
 
 find . -name "*" -type d | sort -n -r | while read directory    
-    do
-        dirs_procesados=`echo $directory | tr ' ' '-' | tr '_' '-' | tr '[a-z]' '[A-Z]'`
-        if [ $dirs_procesados == $directory ]
-        	then
-          	echo "directorio: $CYAN $directory ya esta procesado ... sin cambios"
-        	else
-            mv "$directory" "$dirs_procesados"
-            echo "renombrando Directorio: $VERDE $directory a $ROJO $dir_procesados"
-        fi
-    done
+do
+    dirs_procesados=`echo $directory | tr ' ' '-' | tr '_' '-' | tr '[a-z]' '[A-Z]'`
+    if [ $dirs_procesados == $directory ]
+    then
+        echo "directorio: $CYAN $directory ya esta procesado ... sin cambios"
+    else
+        mv "$directory" "$dirs_procesados"
+        echo "renombrando Directorio: $VERDE $directory a $ROJO $dir_procesados"
+    fi
+done
